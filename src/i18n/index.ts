f85c543a893419711a69bd77acb91390da4e9bee
Michael Zhao <44533763+Pistonight@users.noreply.github.com>
2026-05-09 12:28:41 -0700

    feat: decentralized loader registration (#10)

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,4 +2,5 @@ export * from "./state.ts";
 export * from "./helper.ts";
 export * from "./integration.ts";
 export * from "./init.ts";
+export * from "./loaders.ts";
 export * from "./types.ts";
---
+++ src/i18n/loaders.ts
@@ -0,0 +1,38 @@
+import type { LoadLanguageFn } from "./types.ts";
+import i18next from "i18next";
+
+import { log } from "#util";
+
+const namedspacedLoaders: Map<string, LoadLanguageFn> = new Map();
+const namespacedAwaiters: Map<string, ((loader: LoadLanguageFn) => void)[]> = new Map();
+
+/** Register a translation loader for a namespace */
+export const registerTranslationLoader = (namespace: string, loader: LoadLanguageFn) => {
+    if (namedspacedLoaders.has(namespace)) {
+        log.error(`translation namespace '${namespace}' is already registered`);
+        return;
+    }
+    namedspacedLoaders.set(namespace, loader);
+    const awaiters = namespacedAwaiters.get(namespace);
+    if (!awaiters) {
+        return;
+    }
+    namespacedAwaiters.delete(namespace);
+    awaiters.forEach((x) => x(loader));
+    void i18next.loadNamespaces(namespace);
+};
+
+export const getTranslationLoaderForNamespace = (namespace: string): Promise<LoadLanguageFn> => {
+    const loader = namedspacedLoaders.get(namespace);
+    if (loader) {
+        return Promise.resolve(loader);
+    }
+    return new Promise((resolve) => {
+        const awaiters = namespacedAwaiters.get(namespace);
+        if (awaiters) {
+            awaiters.push(resolve);
+        } else {
+            namespacedAwaiters.set(namespace, [resolve]);
+        }
+    });
+};
